3a7edce4e54f7a024f3e5ca60db09ecd4ee2cd98
amin10 <manna@mit.edu>
2017-11-16 16:49:30 -0500

    dataloader

--- dataloader.py
+++ dataloader.py
@@ -3,16 +3,27 @@ import numpy as np
 import torch
 import cPickle as pickle
 
-def load_ubuntu_corpus():
-    path='askubuntu-master/text_tokenized.txt.gz'
-    
-    with gzip.open(path) as f:
-        lines = f.readlines()
+class Ubuntu():
+    @staticmethod
+    def load_corpus():
+        """
+        1.1 Setup.
 
-    for line in lines:
-        question_id, question_title, question_body = line.split('\t')
-        yield question_id, question_title, question_body
+        Load Q = {q1, ..., qn}
+        """
+        path='askubuntu-master/text_tokenized.txt.gz'
+        
+        with gzip.open(path) as f:
+            lines = f.readlines()
 
+        for line in lines:
+            question_id, question_title, question_body = line.split('\t')
+            yield question_id, question_title, question_body
 
-for x in load_ubuntu_corpus():
-    print(x[0])
+    def load_training_data():
+        """
+        1.1 Setup
+
+        Load a training set of similar question pairs {(q, q')}
+        """
+        pass

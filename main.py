63f3565011a84b72ced67f181bcf7acb3dac79fc
Aritro Biswas <aritrobiswas@Aritros-MacBook-Pro.local>
2017-11-30 20:19:26 -0500

    debugged nonetype error

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def run_epoch(args, train_loader, model, criterion, optimizer, epoch, mode='trai
         top_results = a list of tuples, in which first element of tuple is the positive or negative label of the question, the second is the score of that question
         new_result = a new result that we want to compare to the top_results. if it is good enough, put it in top_results
         """
+        embed()
         top_results.append(new_result)
         top_results = sorted(top_results, key=lambda i: i[1])[::-1] # sort results from greatest to least score
         top_results = top_results[:num_results] # keep only the top performing num_results number of results
@@ -74,8 +75,8 @@ def run_epoch(args, train_loader, model, criterion, optimizer, epoch, mode='trai
                 current_q_top5 = []
 
             element_score = criterion(query_embed[i_element:i_element+1], other_embed[i_element: i_element+1], torch.abs(ys[i_element: i_element+1]))
-            current_q_top1 = get_top_results(1, current_q_top1, (ys.data[i_element], element_score.data[0]))
-            current_q_top5 = get_top_results(5, current_q_top5, (ys.data[i_element], element_score.data[0]))
+            get_top_results(1, current_q_top1, (ys.data[i_element], element_score.data[0]))
+            get_top_results(5, current_q_top5, (ys.data[i_element], element_score.data[0]))
             # if element_score.data[0] > current_q_best_score:
             #     current_q_best_score = element_score.data[0]
             #     current_q_best_score_correct = (ys.data[i_element] == 1)

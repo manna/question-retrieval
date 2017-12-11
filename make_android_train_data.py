6970cb955b04e6a68059cba1deac7f40417a8c99
Aritro Biswas <aritrobiswas@Aritros-MacBook-Pro.local>
2017-12-10 23:07:31 -0500

    debugging android train data issue

--- make_android_train_data.py
+++ make_android_train_data.py
@@ -2,6 +2,7 @@ from dataloader import Ubuntu
 corpus_path='Android-master/corpus.tsv.gz'
 CORPUS = Ubuntu.load_corpus(path=corpus_path)
 examples_per_query = 20
+example_divisor = 4
 
 pos = dict()
 for i, k in enumerate(CORPUS):
@@ -12,11 +13,16 @@ for i, k in enumerate(CORPUS):
 	    pos[latest_k].append(k)
 
 def preprocess(iters):
+    total_length = iters * len(pos)
+    nearest_length = total_length/example_divisor * example_divisor
+    i = 0
     for _ in range(iters):
         for query, pos_examples in pos.items():
             neg_examples = ''
             line = '\t'.join([query, ' '.join(pos_examples), neg_examples])
-            yield line
+            i += 1
+            if i <= nearest_length:
+                yield line
 
 with open('Android-master/train.txt', 'w') as f:
     f.write('\n'.join(preprocess(6)))
